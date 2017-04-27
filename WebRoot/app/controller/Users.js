c0cb63b1ed4cbd58d6a026724a60413f3ca04164
mrossZ <635981836@qq.com>
2017-04-27 11:22:21 +0800

    ext MVC 架构搭建

---
+++ WebRoot/app/controller/Users.js
@@ -0,0 +1,14 @@
+Ext.define('AM.controller.Users',{
+	extend:'Ext.app.Controller',
+	
+	init:function(){
+		this.control({
+			'viewport > panel':{
+				render:this.onPanelRendered()
+			}
+		});
+	},
+	onPanelRendered: function(){
+		console.log('The panel was rendered.');
+	}
+});
