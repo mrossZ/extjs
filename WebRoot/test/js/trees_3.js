502b46601ac5f433948a34eaf2eacc3a08ae6837
mrossZ <635981836@qq.com>
2017-05-02 10:33:56 +0800

    动态增加tree节点数据；

--- WebRoot/test/js/trees_3.js
+++ WebRoot/test/js/trees_3.js
@@ -7,9 +7,43 @@ Ext.onReady(init);
 
 function init() {
 
+	Ext.define('Person',{
+		extend:'Ext.data.Model',
+		fields:[
+			{ name:'id', type:'int' },
+			{ name:'name', type:'string' }
+		],
+		proxy:{
+			type:'ajax',
+			api:{
+				create:'createPersons',
+				read:'readPersons',
+				update:'updatePersons',
+				destory:'destoryPersons'
+			}
+		}
+		
+	});
 
-// Trees  Loading Data 
+	var store=Ext.create('Ext.data.TreeStore',{
+		model:'Person',
+		root:{
+			name:'people',
+			expanded:true
+		}
+	});
 	
+	Ext.create('Ext.tree.Panel',{
+		renderTo:Ext.getBody(),
+		width:300,
+		height:200,
+		title:People,
+		store:store,
+		columns:[
+			{ xtype:'treecolumn',header:'Name',dataIndex:'name',flex:1}
+		]
+	});
+
 	
 	
 };
